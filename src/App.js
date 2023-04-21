4d36a273b22da37ec7b626aa7c7e392873fe84b2
Poojalaksh2002 <lakshmipooja232@gmail.com>
2023-04-21 21:31:15 +0530

    BB Task Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import { images as image, bags, bagData } from "./utils/constants.utils";
+
+let bagObj = bagData;
+
+function Shop() {
+  let getLocal = localStorage.getItem("shopList");
+
+  function remove(e) {
+    let arr = [];
+    arr.push(e);
+    let val = [];
+    arr.map((data) => {
+      bagObj.filter((e) => {
+        if (data.id !== e.id) {
+          val.push(e);
+        }
+        return null;
+      });
+      return null;
+    });
+    bagObj = val;
+
+    const localData = JSON.stringify(bagObj);
+    localStorage.setItem("shopList", localData);
+    getLocal = localStorage.getItem("shopList");
+    window.location.reload();
+  }
+
+  useEffect(() => {
+    if (!getLocal) {
+      const localData = JSON.stringify(bagObj);
+      localStorage.setItem("shopList", localData);
+    }
+  }, []);
+
+  if (getLocal) {
+    bagObj = JSON.parse(getLocal);
+  }
+
+  if (Object.keys(bagObj).length) {
+    return (
+      <>
+        <div className="container">
+          {bagObj.map((data) => {
+            return (
+              <div className="wrapper" style={{ backgroundColor: `${data?.bg}` }}>
+                  <div className="tab_wrapper">
+                    <div className="left_tab_container">Lipault</div>
+                    <div className="right_tab_container">
+                      <div className="tab_container kate">
+                        <div className="image_container">
+                          <img
+                            className="icon"
+                            src={image?.user}
+                            alt="profile"
+                          />
+                        </div>
+                        <h5 className="user_name">Kate</h5>
+                      </div>
+                      <div className="tab_container category">
+                        <div className="image_container">
+                          <img
+                            className="icon bag"
+                            src={image?.bag}
+                            alt="profile"
+                          />
+                        </div>
+                        <h5 className="user_name">Category</h5>
+                      </div>
+                      <div className="tab_container cart">
+                        <div className="right_user_name">Cart</div>
+                        <div className="image_container">
+                          <img
+                            className="icon"
+                            src={image?.cart}
+                            alt="profile"
+                          />
+                        </div>
+                      </div>
+                      <div className="tab_container login">
+                        <div className="login_name">Login</div>
+                        <div className="image_container">
+                          <img
+                            className="icon"
+                            src={image?.user}
+                            alt="profile"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                <div className="details">
+                  <div className="details_row">
+                    <div className="bag_desc">Design Style</div>
+                    <div className="bag_details">{data.bagName}</div>
+                    <div className="price">{data.price}</div>
+                    <div className="variation">
+                      {bags.map((e) => {
+                        if (e !== data.bagImage) {
+                          return <img className="var_image" src={e} alt="bag" />;
+                        }
+                        return null;
+                      })}
+                    </div>
+                    <div className="button">
+                      <button className="buy_now">Add to Cart</button>
+                      <div onClick={() => remove(data)} className="image_container">
+                        <img className="icon" src={image?.bin} alt="bin" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="image_row">
+                    <img className="main_bag" src={data.bagImage} alt="bag" />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h2 style={{ textAlign: "center", fontSize: "4em" }}>
+          Oops! Something went wrong
+        </h2>
+        <p style={{ textAlign: "center", fontSize: "22px" }}>
+          Do delete LocalData in localStorage {"&"} then Refresh
+        </p>
+      </>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Shop />
+    </>
   );
 }
 
---
+++ src/utils/constants.utils.js
@@ -0,0 +1,69 @@
+import bag from "../assets/green.png";
+import bag1 from "../assets/red.png";
+import bag2 from "../assets/blue.png";
+import bag3 from "../assets/pink.png";
+import bag4 from "../assets/yellow.png";
+import bag5 from "../assets/brown.png";
+import bag6 from "../assets/download.png";
+import user from "../assets/user.png";
+import bin from "../assets/dustbin.png";
+import cart from "../assets/cart.png";
+
+export const bags = [bag1, bag2, bag3, bag4, bag5, bag6];
+
+export const images = {
+  bag,
+  bag1,
+  bag2,
+  bag3,
+  bag4,
+  bag5,
+  user,
+  bin,
+  cart,
+};
+
+export const bagData = [
+  {
+    id: 1,
+    bagName: "Plume Avenue",
+    price: "$19.99",
+    bagImage: bag,
+    bg: "#707358",
+  },
+  {
+    id: 2,
+    bagName: "Plume Elegence",
+    price: "$19.99",
+    bagImage: bag1,
+    bg: "#f37068",
+  },
+  {
+    id: 3,
+    bagName: "Seince Off",
+    price: "$19.99",
+    bagImage: bag2,
+    bg: "#9ab7df",
+  },
+  {
+    id: 4,
+    bagName: "Wild-Craft",
+    price: "$19.99",
+    bagImage: bag3,
+    bg: "#eaccc4",
+  },
+  {
+    id: 5,
+    bagName: "American Tour",
+    price: "$19.99",
+    bagImage: bag4,
+    bg: "#FFD300",
+  },
+  {
+    id: 6,
+    bagName: "Puma",
+    price: "$19.99",
+    bagImage: bag5,
+    bg: "#907773",
+  },
+];
